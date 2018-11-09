cc82cc872e18332d94fc21c861d297235456dfa7
Henry Togwell <2259665t@student.gla.ac.uk>
2018-11-09 15:02:09 +0000

    Beautified

--- scripts.js
+++ scripts.js
@@ -1,66 +1,65 @@
-function clicked(){
+function clicked() {
 
-var infos = document.getElementsByClassName("info"); 
+    var infos = document.getElementsByClassName("info");
 
-var swap = 0;
-var first = true;
-var top = 0;
+    var swap = 0;
+    var first = true;
+    var top = 0;
 
-for (i = 0; i < infos.length; i++) {
-if (first == true){
-top = $('#'+infos[i].id).css("top");
-first = false;
+    for (i = 0; i < infos.length; i++) {
+        if (first == true) {
+            top = $('#' + infos[i].id).css("top");
+            first = false;
+        }
+
+        swap = i + 1;
+        if (swap >= infos.length) {
+            swap = 0;
+        }
+        if (swap == 0) {
+            $('#' + infos[i].id).css('top', top);
+        } else {
+            $('#' + infos[i].id).css('top', $('#' + infos[swap].id).css("top"));
+        }
+    }
+
 }
 
-swap = i + 1;
-if (swap >= infos.length){
-swap = 0;
-}
-if (swap == 0){
-$('#'+infos[i].id).css('top', top);
-}else{
-$('#'+infos[i].id).css('top', $('#'+infos[swap].id).css("top"));
-}
+function upload_gpx_pressed() {
+    var read = new FileReader();
+    file = $("#input").prop('files')[0];
+
+    read.readAsBinaryString(file);
+
+    read.onloadend = function() {
+        xmlDoc = $.parseXML(read.result);
+        display_xml(xmlDoc)
+    }
+    $("#form").css("visibility", "hidden");
 }
 
-}
+function display_xml(xml) {
 
-function upload_pressed(){
-file = $("#input").prop('files')[0];
-$("#display").attr("src",URL.createObjectURL(file));
-}
-
-function upload_gpx_pressed(){
-var read = new FileReader();
-file = $("#input").prop('files')[0];
-
-read.readAsBinaryString(file);
-
-read.onloadend = function(){
-    xmlDoc = $.parseXML( read.result );
-    display_xml(xmlDoc)
-}
-}
-
-function display_xml(xml){
-
-var track = $(xml).find("trk").first();
-$("#track_name").html($(track).find("name"));
-var points = $(track).find("trkpt");
-var linePoints = [];
-var heartRates = 0;
-var ratesCount = 0;
-for(var i = 0; i < points.length; i++){
-linePoints.push([points[i].getAttribute('lat'), points[i].getAttribute('lon')]);
-heartRates += $(points[i]).find("gpxtpx:hr").first();
-ratesCount+=1;
-}
-var polyline = L.polyline(linePoints, {color: 'red'}).addTo(mymap);
-// zoom the map to the polyline
-mymap.fitBounds(polyline.getBounds());
-//alert(heartRates);
-//alert(ratesCount);
-//alert(heartRates/ratesCount);
-$('#heart_rate').html(heartRates/ratesCount);
-$("#mapid").css("visibility","visible");
+    var track = $(xml).find("trk").first();
+    $("#track_name").html($(track).find("name"));
+    var points = $(track).find("trkpt");
+    var linePoints = [];
+    var heartRates = 0;
+    var ratesCount = 0;
+    for (var i = 0; i < points.length; i++) {
+        linePoints.push([points[i].getAttribute('lat'), points[i].getAttribute('lon')]);
+        heartRates += $(points[i]).find("gpxtpx:hr").first();
+        alert($(points[i]).find("gpxtpx:hr").first());
+        ratesCount += 1;
+    }
+    var polyline = L.polyline(linePoints, {
+        color: 'red'
+    }).addTo(mymap);
+    // zoom the map to the polyline
+    mymap.fitBounds(polyline.getBounds());
+    alert(heartRates);
+    alert(ratesCount);
+    alert(heartRates / ratesCount);
+    $('#heart_rate').html(heartRates / ratesCount);
+    $("#mapid").css("visibility", "visible");
 }
